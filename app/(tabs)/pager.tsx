b5cba5f3191f4393bf4a7a5fac06e20f8f614443
young-st511 <asdk511@gmail.com>
2025-06-04 14:14:25 +0900

    assets: 템플릿 파일들 추가

---
+++ app/(tabs)/pager.tsx
@@ -0,0 +1,17 @@
+import { Riveo } from '@/components/Riveo'
+import React from 'react'
+
+
+interface Props {
+  ////
+}
+
+const pager: React.FC<Props> = (props) => {
+
+  
+  return (
+    <Riveo />
+  )
+}
+
+export default pager
